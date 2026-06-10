d6ef2def7a1d0865e5312d5eecff2e68fbeb2ea9
keshav2108 <keshavraj2110@gmail.com>
2026-06-10 04:21:48 +0000

    Server file update

--- server.js
+++ server.js
@@ -1,38 +1,32 @@
-
 const express = require('express');
 const http = require('http');
+const path = require('path');
 const { Server } = require('socket.io');
-const cors = require('cors');
 
 const app = express();
-app.use(cors());
 
 const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: "*"
-  }
-});
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send('Messaging server is running');
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 io.on('connection', (socket) => {
-  console.log('User connected:', socket.id);
+  console.log('User connected');
 
   socket.on('send_message', (data) => {
     io.emit('receive_message', data);
   });
 
   socket.on('disconnect', () => {
-    console.log('User disconnected:', socket.id);
+    console.log('User disconnected');
   });
 });
 
-const PORT = 3000;
-
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
 });
